f093ced1e91cb24ea47581ffd93bc9058a6fc4c4
himaniag10 <himaniagarwal1006@gmail.com>
2026-02-09 04:06:36 +0000

    Updates utils folder

---
+++ server/src/utils/hash.js
@@ -0,0 +1,13 @@
+const bcrypt=require("bcrypt");
+
+function hashPassword(password){
+    return bcrypt.hash(password,10)
+}
+
+function comparePassword(password,hash){
+    return bcrypt.compare(password,hash);
+}
+
+module.exports={
+    hashPassword,comparePassword
+}
