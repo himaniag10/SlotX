b32ca29d4303c1eefcece65d3d2405d3995d3629
himaniag10 <himaniagarwal1006@gmail.com>
2026-02-09 17:49:07 +0000

    jwt.js file added

--- server/src/utils/jwt.js
+++ server/src/utils/jwt.js
@@ -1,1 +1,26 @@
-const jwt=require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const signAccessToken = (payload) =>
+    jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    });
+
+const signRefreshToken = (payload) =>
+    jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    });
+
+const verifyAccessToken = (token) =>
+    jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+
+const verifyRefreshToken = (token) =>
+    jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+
+
+module.exports = {
+    signAccessToken,
+    signRefreshToken,
+    verifyAccessToken,
+    verifyRefreshToken,
+};
