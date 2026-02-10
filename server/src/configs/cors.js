31d267d129ebd41874f91bdac5840816e407f315
himaniag10 <himaniagarwal1006@gmail.com>
2026-02-10 08:15:24 +0000

    cors.js modified

--- server/src/configs/cors.js
+++ server/src/configs/cors.js
@@ -1,16 +1,17 @@
 const cors = require("cors");
 require("dotenv").config();
+
 const allowedOrigins = [
     process.env.FRONTEND_LOCAL_URL,
     process.env.FRONTEND_SERVER_URL,
 ];
+
 const corsOptions = {
     origin: function (origin, callback) {
         if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true);
         }
         else {
-            console.warn(`❌ Blocked by CORS: ${origin}`);
             callback(new Error("Not allowed by CORS"));
         }
     },
@@ -18,4 +19,5 @@ const corsOptions = {
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
 };
+
 module.exports = cors(corsOptions);
